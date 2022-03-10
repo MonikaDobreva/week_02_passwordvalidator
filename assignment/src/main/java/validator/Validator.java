40c795f25baf8982c35f0f284af368c9eaba639c
MonikaDobreva <m.dobreva@student.fontys.nl>
2022-03-10 12:34:36 +0100

    validator impl

--- assignment/src/main/java/validator/Validator.java
+++ assignment/src/main/java/validator/Validator.java
@@ -1,12 +1,14 @@
 package validator;
 
 //import static java.lang.Character.;
+import java.util.ArrayList;
 import java.util.EnumSet;
+import java.util.stream.IntStream;
+import static java.lang.Character.isAlphabetic;
+import static java.lang.Character.isDigit;
+
 import static java.util.stream.Collectors.joining;
-import static validator.Flaw.NODIGIT;
-import static validator.Flaw.NOLOWER;
-import static validator.Flaw.NOUPPER;
-import static validator.Flaw.TOO_SHORT;
+import static validator.Flaw.*;
 
 /**
  * Password validator using lambdas and maps.
@@ -17,7 +19,104 @@ public class Validator {
 
     void validate( String password ) {
         EnumSet<Flaw> flaws = EnumSet.allOf( Flaw.class );
-        
+
+        String totalFlaws = "";
+        totalFlaws += noUpperCaseLetterCheck(password);
+        totalFlaws += noLowerCaseLetterCheck(password);
+        totalFlaws += noDigitCheck(password);
+        totalFlaws += noSpecialCharCheck(password);
+        totalFlaws += tooShortCheck(password);
+        //add rest of the cases
+
+        String exceptionMessage = "";
+        if (totalFlaws.contains("U")) {
+            exceptionMessage += NOUPPER.getDescription();
+        }
+        if (totalFlaws.contains("l")) {
+            exceptionMessage += NOLOWER.getDescription();
+        }
+        if (totalFlaws.contains("8")) {
+            exceptionMessage += NODIGIT.getDescription();
+        }
+        if (totalFlaws.contains("#")) {
+            exceptionMessage += NOSPECIAL.getDescription();
+        }
+        if (totalFlaws.contains("s")) {
+            exceptionMessage += TOO_SHORT.getDescription();
+        }
+
+        if (exceptionMessage.length() > 0) {
+            throw new InvalidPasswordException(exceptionMessage);
+        }
+    }
+
+    private String tooShortCheck(String password) {
+        if (password.length() < 10) {
+            return "s";
+        }
+
+        return "";
+    }
+
+    private String noSpecialCharCheck(String password) {
+        String totalFlawsFound = "";
+
+        ArrayList<Character> specialChars = new ArrayList<>();
+
+        for (int j : SpecialChars.getSpecialChars()) {
+            specialChars.add((char) j);
+        }
+
+        for (Character specialChar : specialChars) {
+            if (password.indexOf(specialChar) != -1) {
+                return "";
+            }
+        }
+
+        totalFlawsFound += '#';
+        return totalFlawsFound;
+    }
+
+    private String noDigitCheck(String password) {
+        String totalFlawsFound = "";
+        char[] passwordCharacters = password.toCharArray();
+
+        for (int i = 0; i < password.length(); i++) {
+            if (Character.isDigit(passwordCharacters[i])) {
+                return "";
+            }
+        }
+
+        totalFlawsFound += '8';
+        return totalFlawsFound;
+    }
+
+    private String noLowerCaseLetterCheck(String password) {
+        String totalFlawsFound = "";
+        char[] passwordCharacters = password.toCharArray();
+
+        for (int i = 0; i < password.length(); i++) {
+            if (Character.isLowerCase(passwordCharacters[i])) {
+                return "";
+            }
+        }
+
+        totalFlawsFound += 'l';
+        return totalFlawsFound;
+    }
+
+    private String noUpperCaseLetterCheck(String password) {
+        String totalFlawsFound = "";
+        char[] passwordCharacters = password.toCharArray();
+
+        for (int i = 0; i < password.length(); i++) {
+            if (Character.isUpperCase(passwordCharacters[i])) {
+                return "";
+            }
+        }
+
+        totalFlawsFound += 'U';
+        return totalFlawsFound;
     }
 
 }
--- assignment/src/test/java/validator/ValidatorTest.java
+++ assignment/src/test/java/validator/ValidatorTest.java
@@ -4,10 +4,18 @@ import java.util.Arrays;
 import java.util.EnumSet;
 import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.assertj.core.api.SoftAssertions;
 import org.assertj.core.api.ThrowableAssert.ThrowingCallable;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 
 /**
  * Test validator with Parameterized test.
@@ -16,7 +24,4 @@ import org.junit.jupiter.params.provider.CsvSource;
  */
 public class ValidatorTest {
 
-    // Write parameterized test method
-    
-    // Write parameterized test method
 }
